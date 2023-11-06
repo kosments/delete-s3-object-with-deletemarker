6d2db6f19cbbecc707c482a23839745686407b2f
kosments <koubsn@gmail.com>
2023-11-06 20:23:22 +0900

    sh fix

--- delete_object.sh
+++ delete_object.sh
@@ -5,13 +5,12 @@ BUCKET_NAME="your-bucket-name"
 SEARCH_STRING="dummy"
 
 # 指定したバケットからオブジェクトのリストを取得
-aws s3api list-object-versions --bucket $BUCKET_NAME | jq -r '.Versions[] | select(.Key | contains("'$SEARCH_STRING'"))' | while read -r line;
-  do
-    # オブジェクトキーとバージョンIDを取得
-    KEY_NAME=$(echo $line | jq -r .Key)
-    VERSION_ID=$(echo $line | jq -r .VersionId)
+for VERSION_ID in $(aws s3api list-object-versions --bucket $BUCKET_NAME --query "Versions[?contains(Key,'$SEARCH_STRING')].VersionId" --output text)
+do
+  # オブジェクトキーとバージョンIDを取得
+  KEY_NAME=$(aws s3api list-object-versions --bucket $BUCKET_NAME --query "Versions[?VersionId=='$VERSION_ID'].Key" --output text)
 
-    # 検索文字列を含むオブジェクトを削除
-    echo "Deleting $KEY_NAME"
-    aws s3api delete-object --bucket $BUCKET_NAME --key "$KEY_NAME" --version-id $VERSION_ID
-  done
+  # 検索文字列を含むオブジェクトを削除
+  echo "Deleting $KEY_NAME"
+  aws s3api delete-object --bucket $BUCKET_NAME --key "$KEY_NAME" --version-id $VERSION_ID
+done
